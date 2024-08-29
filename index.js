39ca5ef8d9483f440fd04777058105fa879947df
Leonardo <martinezjleonardo@gmail.com>
2024-08-29 10:45:48 -0300

    acomodando

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import Express from "express";
-import fs from "fs";
+import data from "./data.js";
 import { config } from "dotenv";
 config();
 
@@ -8,16 +8,7 @@ const app = Express();
 app.get("/:id", (req, res) => {
     const id = req.params.id;
 
-    fs.readFile("data.json", "utf8", (error, data) => {
-        if (error) {
-            console.log(error);
-            return;
-        }
-
-        const dataRecibida = JSON.parse(data);
-
-        res.send(dataRecibida[id]);
-    });
+    res.send(data[id]);
 });
 
 app.listen(process.env.PORT || 3000, () => {
